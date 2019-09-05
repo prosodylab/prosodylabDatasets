c43e8f882fd830e9c3b1e2b5c0c475ceb7423a56
chael <mchlwgnr@gmail.com>
2019-09-05 11:52:17 -0400

    initial commit

---
+++ R/hello.R
@@ -0,0 +1,18 @@
+
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+
+#alternatives = read.csv('alternatives.csv',sep=',')
+#tapping = read.csv('tapping.csv',sep=',')
+#givenness = read.csv('givenness.csv',sep=',')
